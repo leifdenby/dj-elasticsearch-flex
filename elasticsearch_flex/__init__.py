369306fa474bc25178785358cf46c8f508f31953
Prashant Sinha <prashant+git@noop.pw>
2016-12-12 23:32:24 +0530

    Implemented the IndexedModel class for defining indices

--- elasticsearch_flex/__init__.py
+++ elasticsearch_flex/__init__.py
@@ -1,6 +1,10 @@
-__version__ = '0.1.0'
-
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+__version__ = '0.1.0'
 
 
 default_app_config = 'elasticsearch_flex.apps.ElasticsearchFlexConfig'
+
+if hasattr(settings, 'ElasticsearchFlex'):
+    'host' in settings.ElasticsearchFlex
---
+++ elasticsearch_flex/indexes.py
@@ -0,0 +1,61 @@
+from six import add_metaclass
+from elasticsearch_dsl import DocType
+from elasticsearch_dsl.field import Field
+
+
+class Indexable(type):
+    '''Defines an introspection type for Indexable Models.'''
+    def __new__(cls, name, bases, namespace):
+        fields = []
+        # We introspect and collect the names of all the Fields in Index
+        # to facilitate differentiating field attribute from non-field attribute
+        # later.
+        for k, x in namespace.items():
+            if isinstance(x, Field):
+                fields.append(k)
+        # Append field-name information from all the Base classes.
+        for base_index in filter(lambda x: issubclass(x, IndexedModel), bases):
+            fields.extend(base_index._fields)
+        # Insert fields into this class's namespace.
+        namespace['_fields'] = fields
+
+        # TODO: Assert sanity of extension class
+        # -- Implement checking get_queryset and get_model implementations.
+        return super(Indexable, cls).__new__(cls, name, bases, namespace)
+
+
+@add_metaclass(Indexable)
+class IndexedModel(DocType):
+    '''Base class for declaring Index for a Model.
+
+    This class is an extension of Elasticsearch-DSL's DocType. For the fields,
+    as a convention, it is assumed that the model has the attribute of the
+    same name. This default behaviour can be controlled by implementing a
+    `prepare_<FieldName>` method, accepting the object instance.
+    '''
+    def get_queryset(self):
+        return self.queryset
+
+    def get_object(self):
+        qs = self.get_queryset()
+        return qs.get(pk=self._id)
+
+    @property
+    def object(self):
+        return self.get_object()
+
+    def _prepare(self):
+        obj = self.get_object()
+        for field in self._fields:
+            try:
+                prep_method = getattr(self, 'prepare_{0}'.format(field))
+                setattr(self, field, prep_method(obj))
+            except AttributeError:
+                val_from_obj_attr = getattr(obj, field)
+                setattr(self, field, val_from_obj_attr)
+
+    @classmethod
+    def prepare(cls, pk):
+        obj = cls(_id=pk)
+        obj._prepare()
+        return obj
