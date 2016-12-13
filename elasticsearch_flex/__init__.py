72de3eef9208a39116ad7f2876d11313050eaf8a
Prashant Sinha <prashant+git@noop.pw>
2016-12-13 15:20:03 +0530

    Added module logger

--- elasticsearch_flex/__init__.py
+++ elasticsearch_flex/__init__.py
@@ -1,8 +1,11 @@
+import logging
+
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
 __version__ = '0.1.0'
 
+logger = logging.getLogger('elasticsearch_flex')
 
 default_app_config = 'elasticsearch_flex.apps.ElasticsearchFlexConfig'
 
