2f4b820127ee4007179a1acd97500587b56780e4
Prashant Sinha <prashant+git@noop.pw>
2016-12-13 17:29:46 +0530

    Added Management command implementation

--- elasticsearch_flex/management/commands/flex_setup_index.py
+++ elasticsearch_flex/management/commands/flex_setup_index.py
@@ -1,12 +1,27 @@
 # coding: utf-8
+import hues
 from tqdm import tqdm
 
 from django.core.management.base import BaseCommand, CommandError
 
+from elasticsearch_flex.indexes import registered_indices
+
 
 class Command(BaseCommand):
-    args = '<nb>'
-    help = 'Generate events with random data'
+    help = 'Setup Search Index'
 
-    def handle(self, nb=20, *args, **options):
-        pass
+    def handle(self, *args, **options):
+        indices = registered_indices()
+        if len(indices):
+            hues.info('Discovered', len(indices), 'Indexes')
+        else:
+            hues.warn('No search indexe found')
+        for i, index in enumerate(indices, 1):
+            hues.info('==> Initializing', index)
+            # index.init()
+            hues.info('--> Indexing from', index.model)
+            for obj in tqdm(index.queryset):
+                doc = index.init_using_pk(obj.pk)
+                doc.prepare()
+                doc.save()
+            hues.success('--> {0}/{1} indexed.'.format(i, len(indices)))
