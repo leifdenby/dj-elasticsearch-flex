9638e1c8c95602a0bead6ce269a9070ea0a36d48
Prashant Sinha <prashant+git@noop.pw>
2016-12-13 17:30:01 +0530

    Auto-discover the indices

--- elasticsearch_flex/apps.py
+++ elasticsearch_flex/apps.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8
+from importlib import import_module
+
+from django.conf import settings
 from django.apps import AppConfig
 
 
 class ElasticsearchFlexConfig(AppConfig):
     name = 'elasticsearch_flex'
+
+    def ready(self):
+        # Discover the modules
+        import elasticsearch_flex.signals
+        for app_name in settings.INSTALLED_APPS:
+            module = '{}.search_indexes'.format(app_name)
+            try:
+                import_module(module)
+            except ImportError:
+                pass
