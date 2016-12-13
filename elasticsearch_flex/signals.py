bd98fc81247bbb4ce1f02a396adc35f9979c3dc2
Prashant Sinha <prashant+git@noop.pw>
2016-12-13 17:29:23 +0530

    More descriptive signal identifier

--- elasticsearch_flex/signals.py
+++ elasticsearch_flex/signals.py
@@ -1,12 +1,16 @@
+import logging
+
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from . import tasks
 from .indexes import get_index_for_model
 
+logger = logging.getLogger('elasticsearch_flex.signals')
+
 
 @receiver(post_save)
-def on_update_or_create(sender, instance, created, **kwargs):
+def sync_elasticsearch_on_model_update_or_create(sender, instance, created, **kwargs):
     try:
         ix = get_index_for_model(sender)
     except KeyError:
@@ -16,7 +20,7 @@ def on_update_or_create(sender, instance, created, **kwargs):
 
 
 @receiver(post_delete)
-def on_model_delete(sender, instance, **kwargs):
+def sync_elasticsearch_on_model_delete(sender, instance, **kwargs):
     try:
         ix = get_index_for_model(sender)
     except KeyError:
@@ -25,4 +29,7 @@ def on_model_delete(sender, instance, **kwargs):
         tasks.delete_indexed_document.delay(ix, instance.pk)
 
 
-__all__ = ('on_update_or_create', 'on_model_delete')
+__all__ = (
+    'sync_elasticsearch_on_model_update_or_create',
+    'sync_elasticsearch_on_model_delete',
+)
