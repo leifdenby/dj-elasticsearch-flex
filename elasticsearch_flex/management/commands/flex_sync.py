818ccc810e2ab2db6730477bc49b5da284019fe0
Prashant Sinha <prashant+git@noop.pw>
2016-12-22 15:04:31 +0530

    Added standalone index sync command

---
+++ elasticsearch_flex/management/commands/flex_sync.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+import hues
+
+from django.core.management.base import BaseCommand
+
+from elasticsearch_dsl.connections import connections
+from elasticsearch_flex.indexes import registered_indices
+
+
+class Command(BaseCommand):
+    help = 'Sync search indices, templates, and scripts.'
+
+    def handle(self, *args, **options):
+        indices = registered_indices()
+        connection = connections.get_connection()
+        hues.info('Using connection', connection)
+        if len(indices):
+            hues.info('Discovered', len(indices), 'Indexes')
+        else:
+            hues.warn('No search index found')
+        for i, index in enumerate(indices, 1):
+            hues.info('==> Initializing', index.__name__)
+
+            index_name = index._doc_type.index
+            connection.indices.close(index_name)
+            index.init()
+            connection.indices.open(index_name)
+
+            hues.success('--> Done {0}/{1}'.format(i, len(indices)))
