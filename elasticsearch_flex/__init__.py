756aaefc79440d970422e03059b6b3438278c99a
Prashant Sinha <prashant+git@noop.pw>
2016-12-22 15:02:24 +0530

    Aliased connections in init

--- elasticsearch_flex/__init__.py
+++ elasticsearch_flex/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
+from elasticsearch_dsl.connections import connections
 
 __version__ = '0.1.0'
 
@@ -11,3 +11,5 @@ default_app_config = 'elasticsearch_flex.apps.ElasticsearchFlexConfig'
 
 if hasattr(settings, 'ElasticsearchFlex'):
     'host' in settings.ElasticsearchFlex
+
+__all__ = ('connections',)
