ceb07bfd6a0b81a48e0eb93bf6cc84064c18ecc8
Prashant Sinha <prashant+git@noop.pw>
2017-01-15 17:06:13 +0530

    Updated Document accessor instances

--- elasticsearch_flex/query.py
+++ elasticsearch_flex/query.py
@@ -1,96 +1,78 @@
-'''Django Compatible Search Queryset
+'''Search Queryset
 
-This search queryset is a subset of django queryset wrapping over the low level
-elasticsearch queries.
+This search queryset wraps the low level elasticsearch queries, and acts as
+a transparent interface between elasticsearch documents and corresponding
+django model objects.
 '''
+import elasticsearch_dsl
+
+from collections import namedtuple
+from elasticsearch_dsl.search import Search
+from elasticsearch_dsl import response
+from elasticsearch_dsl.utils import AttrList, AttrDict
+
 from . import connections
 
 
-class BaseSearchQuerySet(object):
-    def __init__(self, index, **kwargs):
+def from_queryset(hits, qs):
+    ids = [hit._id for hit in hits]
+    objects = qs.in_bulk(ids)
+    keytype = type(objects.keys()[0])
+    pks = list(map(keytype, ids))
+    return pks, [objects[i] for i in pks]
+
+
+class ModelSearch(Search):
+    @property
+    def objects(self):
+        docs = list(self)
+        if len(docs):
+            # Use one document for introspection
+            doczero = docs[0]
+            qs = doczero.get_queryset()
+            _, objects = from_queryset(docs, qs)
+            return objects
+        return []
+
+
+class TemplateSearch(ModelSearch):
+    def execute(self, ignore_cache=False):
+        if ignore_cache or not hasattr(self, '_response'):
+            es = connections.get_connection(self._using)
+            payload = {
+                'id': self._extra['t_name'],
+                'params': self._params,
+            }
+
+            self._response = self._response_class(
+                self,
+                es.search_template(
+                    index=self._index,
+                    doc_type=self._doc_type,
+                    body=payload,
+                )
+            )
+        return self._response
+
+
+class DocAccessors(object):
+    def __init__(self, index):
         self.index = index
-        self._params = kwargs
-        self._meta = {}
 
     @property
-    def connection(self):
-        return connections.get_connection()
-
-    def _init_objects(self):
-        hits = self._results['hits']['hits']
-        if len(hits) == 0:
-            return
-
-        # TODO -- maybe keep the raw scores?
-        ids = [hit['_id'] for hit in hits]
-        qs = self.index().get_queryset()
-
-        self._objects = qs.in_bulk(ids)
-        keytype = type(self._objects.keys()[0])
-        self._pks = list(map(keytype, ids))
-
-    def _perform_search(self):
-        raise NotImplementedError('Must use a subclass.')
-
-    def _clone_with(self, params):
-        qs = self.__class__(self.index, **self._params)
-        qs._meta = self._meta.copy()
-        qs._params.update(params)
-        return qs
-
-    def __iter__(self):
-        for i in self._pks:
-            yield self._objects[i]
-
-    def __getitem__(self, key):
-        pk = self._pks[key]
-        return self._objects[pk]
-
-    def __repr__(self):
-        classname = self.__class__.__name__
-
-        return '<{0} index={1}>'.format(classname, self.index)
-
-    def search(self, **kwargs):
-        qs = self._clone_with(kwargs)
-        qs._perform_search()
-        qs._init_objects()
-        return qs
-
-
-class TemplatedSearchQuerySet(BaseSearchQuerySet):
-    def _perform_search(self):
-        payload = {
-            'id': self._meta['template_id'],
-            'params': self._params,
-        }
-        ix_name = self.index._doc_type.index
-        doc_type = self.index._doc_type.name
-
-        self._results = self.connection.search_template(
-            index=ix_name, doc_type=doc_type, body=payload)
-        return self._results
-
-
-class DSLProxySearchQuerySet(BaseSearchQuerySet):
-    def _perform_search(self):
-        pass
-
-    def filter(self, *args, **kwargs):
-        pass
-
-    def sort(self, *args, **kwargs):
-        pass
-
-    def count(self, *args, **kwargs):
-        pass
-
-
-class DocQuerySet(BaseSearchQuerySet):
     def dsl(self):
-        dpqs = DSLProxySearchQuerySet(self.index, **self._params)
+        return ModelSearch().doc_type(self.index)
 
-    def use_template(self, name):
-        tqs = TemplatedSearchQuerySet(self.index, **self._params)
-        tqs._meta['template_id'] = '.'.join([self.index._meta.index, name])
-        return tqs
+    @property
+    def templates(self):
+        index_name = self.index._meta.index
+        available_templates = getattr(self.index._meta, 'query_templates', [])
+
+        FlexTemplates = namedtuple('FlexTemplates', available_templates)
+        dsl = []
+        for _id in available_templates:
+            tid = index_name + '.' + _id
+            s = TemplateSearch(index=index_name, extra={'t_name': tid}, doc_type=self.index)
+            dsl.append(s)
+
+        return FlexTemplates(*dsl)
