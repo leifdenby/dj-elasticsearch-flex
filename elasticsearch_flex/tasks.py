5e976a4298e190dd6847a74e275752d076c38a24
Prashant Sinha <prashant+git@noop.pw>
2017-01-18 15:47:35 +0530

    Update tasks so it always fetches the initial model data from db

--- elasticsearch_flex/tasks.py
+++ elasticsearch_flex/tasks.py
@@ -3,11 +3,7 @@ from celery import shared_task
 
 @shared_task
 def update_indexed_document(index, created, pk):
-    if created:
-        indexed_doc = index.init_using_pk(pk)
-    else:
-        indexed_doc = index.get(id=pk)
-    # Call the prepare method
+    indexed_doc = index.init_using_pk(pk)
     indexed_doc.prepare()
     indexed_doc.save()
 
